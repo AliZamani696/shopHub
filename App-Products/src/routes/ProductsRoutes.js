ed43eaaf39d7ebc117edbc3a23136f89a5359488
ali zamani <azamani081@gmail.com>
2025-11-18 23:38:57 +0330

    create static css file for all views

--- App-Products/src/routes/ProductsRoutes.js
+++ App-Products/src/routes/ProductsRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Router = express.Router();
 const path = require("path")
 
+
 const IndexRoute = require("./IndexRouter");
 Router.get("/Products",IndexRoute.AllProducts)
 Router.get("/FindProduct",IndexRoute.FindProduct);
@@ -9,6 +10,7 @@ Router.put("/EditProduct/:id",IndexRoute.EditProduct);
 Router.delete("/DeleteProduct",IndexRoute.DeleteProduct);
 Router.post("/AddProduct",IndexRoute.AddProduct);
 Router.get("/Category",IndexRoute.Category)
-// Router.use(express.static('/public'))
+
+
 
 module.exports = Router;
