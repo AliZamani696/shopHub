61545fd100100ef84c83afb8ce52ba0b5bfd7699
ali zamani <azamani081@gmail.com>
2025-11-19 19:03:02 +0330

    install multer and hanle a images dir is exsit or not

--- AdminDashboard/uploads/UploadScript.js
+++ AdminDashboard/uploads/UploadScript.js
@@ -1,11 +1,21 @@
 const { mkdirp }=  require('mkdirp')
-
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+const { dir } = require('console');
 // This will create the directory if it doesn't exist
 // mkdirp is idempotent - safe to call even if directory exists
+
+
+
+
 async function ensureDirectory(dirPath) {
         try{
-                await mkdirp(dirPath);
-                console.log('Directory is ready:', dirPath);
+                const uploadsDir = dirPath;
+                if(!fs.existsSync(dirPath)){
+                        const createDir =  await mkdirp(dirPath);
+                        console.log("dir is created!",createDir)
+                }
         }catch(err){
                 console.log(err)
         }
